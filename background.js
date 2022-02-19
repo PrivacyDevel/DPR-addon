a0c628df6675423f6a8dd1f8f6bd12c684677dd7
PrivacyDev <privacydev@cock.lt>
2022-02-19 16:15:11 -0500

    initial commit

---
+++ background.js
@@ -0,0 +1,79 @@
+const CONFIG_URL = "PLACEHOLDER";
+const UPDATE_INTERVAL_MINUTES = 60 * 2;
+
+let g_listeners = [];
+
+function errorHandler(error) {
+	console.error(error);
+}
+
+function createListeners(services) {
+	let listeners = [];
+	for(let service of services) {
+		let listener = details => {
+			let instance = new URL(service.instances[Math.floor(Math.random() * service.instances.length)]);
+			let url = new URL(details.url);
+
+			if(service.documentOnly && details.documentUrl)
+				return;
+
+			url.hostname = instance.hostname;
+			url.protocol = instance.protocol;
+			return {"redirectUrl": url.toString()};
+		};
+		browser.webRequest.onBeforeRequest.addListener(listener, {"urls": service.orig}, ["blocking"]);
+		listeners.push(listener);
+	}
+	return listeners;
+}
+
+async function updateConfig() {
+	
+	console.log("updating service list...");
+
+	let response = await fetch(CONFIG_URL);
+	if(!response.ok) {
+		console.error("updating service failed!");
+		return;
+	}
+	let services = await response.json();
+	await browser.storage.local.set({"config": {"lastUpdated": Date.now(), "services": services}});
+	
+	let listeners = createListeners(services);
+	for(let listener of g_listeners) {
+		browser.webRequest.onBeforeRequest.removeListener(listener);
+	}
+	g_listeners = listeners;
+
+	console.log("service list updated successfully!");
+}
+
+browser.alarms.onAlarm.addListener(alarm => {
+	updateConfig().catch(errorHandler);
+});
+
+(async() => {
+
+	console.log("initializing addon...");
+	
+	let config = (await browser.storage.local.get("config")).config;
+	if(!config) {
+		let services = await (await fetch("services.json")).json();
+		config = {"services": services};
+		await browser.storage.local.set({"config": config});
+		updateConfig().catch(errorHandler);
+	}
+
+	let nextUpdateTimestamp = Math.max((config.lastUpdated || 0) + (1000 * UPDATE_INTERVAL_MINUTES), Date.now() + (1000 * 30));
+	console.log("next update is scheduled for: " + new Date(nextUpdateTimestamp).toString());
+
+	browser.alarms.create({
+		"periodInMinutes": UPDATE_INTERVAL_MINUTES,
+		"when": nextUpdateTimestamp
+	});
+
+	g_listeners = createListeners(config.services);
+
+	console.log("addon initialized successfully!");
+
+})().catch(errorHandler);
